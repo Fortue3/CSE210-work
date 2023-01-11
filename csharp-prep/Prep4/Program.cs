80530fa3891e28e9f5baeb6bde87fede1094830c
Fortue3 <fkwenda69@gmail.com>
2023-01-11 16:12:00 +0200

    prep3 n 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,36 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int num = -1;
+
+        do{
+            Console.WriteLine("Enter a number");
+            string usernumber = Console.ReadLine();
+            num = int.Parse(usernumber);
+            if(num!=0)
+            {
+                numbers.Add(num);
+            }
+        }
+        while(num!=0);
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum+= number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        float avg = ((float)sum)/numbers.Count;
+        Console.WriteLine($"The average of these numbers is: {avg}");
+        int maximum = numbers[0];
+        foreach(int number in numbers)
+        {
+            if(number>maximum)
+            {
+                maximum = number;
+            }
+        }
+         Console.WriteLine($"The maximum number is: {maximum}");
+         
     }
 }
