74e0fd837952edb77f6c906f43dcad5c2b31bd14
Fortue3 <fkwenda69@gmail.com>
2023-02-13 10:36:10 +0200

    develpoer 03

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop03 World!");
+        using System;
+
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,101);
+
+        List<string> verses = new List<string>
+        {
+            "verse",
+            "verse",
+            "verse"
+        };
+    }
+}
     }
 }
